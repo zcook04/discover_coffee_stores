c5856bd138a92db51b91ab7671543985f47a2973
Zachary Cook <zachary.cook@Zacharys-MacBook-Pro.local>
2022-02-19 14:23:43 -0500

    Add toronto store dummy data to map through on main page

--- coffee-shop-locator/pages/index.js
+++ coffee-shop-locator/pages/index.js
@@ -1,19 +1,33 @@
 import styles from '../styles/Home.module.css'
 import Hero from '../components/Hero'
 import CoffeeCard from '../components/CoffeeCard'
+import coffeeStoresData from '../data/coffee-stores.json'
 
-export default function Home() {
+export async function getStaticProps(context) {
+  return {
+    props: {
+      coffeeStores: coffeeStoresData
+    }
+  }
+}
+
+export default function Home(props) {
   return (
     <main className={styles.container}>
       <Hero title="Coffee Shop Locator" subtitle="" />
-      <div className={styles.cardLayout}>
-        <div className={styles.card}><CoffeeCard name="Coffee Store 1" imageUrl="/static/Coffee-Hero.jpeg" href="/coffee-store/store-1" alt="image1" /></div>
-        <div className={styles.card}><CoffeeCard name="Coffee Store 1" imageUrl="/static/Coffee-Hero.jpeg" href="/coffee-store/store-1" alt="image1" /></div>
-        <div className={styles.card}><CoffeeCard name="Coffee Store 1" imageUrl="/static/Coffee-Hero.jpeg" href="/coffee-store/store-1" alt="image1" /></div>
-        <div className={styles.card}><CoffeeCard name="Coffee Store 1" imageUrl="/static/Coffee-Hero.jpeg" href="/coffee-store/store-1" alt="image1" /></div>
-        <div className={styles.card}><CoffeeCard name="Coffee Store 1" imageUrl="/static/Coffee-Hero.jpeg" href="/coffee-store/store-1" alt="image1" /></div>
-      </div>
 
-    </main>
+      {props.coffeeStores.length > 0 && (
+        <>
+          <h2 className={styles.h2}>Toronto Stores</h2>
+          <div className={styles.cardLayout}>
+            {props.coffeeStores.map((store) => {
+              return (
+                <CoffeeCard key={store.id} name={store.name} imageUrl={store.imgUrl} href={`/coffee-store/${store.id}`} alt={store.name} neighborhood={store.neighbourhood} address={store.address} />
+              )
+            })}
+          </div>
+        </>
+      )}
+    </main >
   )
 }
