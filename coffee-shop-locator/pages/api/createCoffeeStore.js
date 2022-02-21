138fb8cb4ff375e48acfb254253fe511079c6076
Zachary Cook <zachary.cook@Zacharys-MacBook-Pro.local>
2022-02-21 11:23:52 -0500

    Refactor airtable api

--- coffee-shop-locator/pages/api/createCoffeeStore.js
+++ coffee-shop-locator/pages/api/createCoffeeStore.js
@@ -29,14 +29,12 @@ const createCoffeeStore = async (req, res) => {
                 ])
 
                 const records = getMinifiedRecords(createRecords)
-                res.status(200).json({ records })
+                return res.status(200).json({ records })
             }
         } catch (err) {
             console.error("Error Creating or Finding Store", err)
             res.status(500).json({ msg: "Error Creating or Finding Store", err })
         }
-
-
 
     } else {
         return res.status(400).json({ msg: 'Bad Request', status: 400 })
